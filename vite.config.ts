c693d2e0b598e838617752f508eec625d573a58c
Baanu Wasiu <baanuwasiu@gmail.com>
2025-04-16 17:50:59 +0100

    Vercel deployment testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@lib': path.resolve(__dirname, './src/lib')
+    }
   },
   base: '/', // Explicit base path
 })
